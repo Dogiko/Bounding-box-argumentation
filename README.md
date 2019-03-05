6de46916776bd05126a457b5f5dd384e743e5160
Dogiko <dogiko@Dogikode-MacBook-Pro.local>
2019-03-05 16:05:20 +0800

    new function crop square contain no box

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Random crop around box for given image and box position.
 
 ## Main function
 
+
+
 ### random_crop_square(img, start, end, n_crop=1, resize=-1, box_in_crop_ratio=(0.707, 1.414), flip=False)
 
 #### Parameters:
@@ -33,3 +35,33 @@ crop_box_center : float (y, x), position of bounding box in croped image(support
 crop_half_weith : float, distence from crop_box_center to left-end or right-end.
 
 crop_half_hight : float, distence from crop_box_center to up-end or down-end.
+
+
+
+### random_crop_square(img, start, end, n_crop=1, resize=-1, avoid_ratio=(0.707, 1.414), flip=False, minimum_length=32, max_try=100)
+
+#### Parameters:
+
+img : array-like, image array (with shape hight x weith x channel)
+
+starts : int numpy array nx2, left-up position of each bounding box, n=number of bounding box
+
+ends : int numpy_array nx2, right-down position of each bounding box, n=number of bounding box
+
+n_crop : number of output croped image
+
+resize : int, resize each output image to assigned value. Do nothing when value < 0.
+
+avoid_ratio : float $(r, R)$, to exclude result which contained some box center and "length of  box / length of square" in this range.
+
+flip : bool, random flip(left <-> right) if True.
+
+minimum_length : int>=0, minimun length(pixel) for area on image.
+
+max_try : int>0, maxmun times of retry if each random result is excluded(see avoid_ratio above).
+
+#### Returns:
+
+List of crop_img
+
+crop_img : array-like, croped image with same channal as input img
--- bounding_box_argumentation.py
+++ bounding_box_argumentation.py
@@ -56,3 +56,38 @@ def random_crop_square(img, start, end, n_crop=1, resize=-1, box_in_crop_ratio=[
         output.append([crop_img, crop_box_center, crop_half_weith, crop_half_hight])
     
     return output
+
+def random_crop_square_nothing(img, starts, ends, n_crop=1, resize=-1, avoid_ratio=[0.707, 1.414], flip=False, minimum_length=32, max_try=100):
+    cheak_type(resize, int)
+    cheak_type(n_crop, int)
+    cheak_interval(avoid_ratio[1], mini=0)
+    cheak_interval(avoid_ratio[0], mini=0, maxi=avoid_ratio[1], left_close=True, right_close=True)
+    cheak_interval(minimum_length, mini=1, maxi=np.min(img.shape[:2]), left_close=True,
+                   message="minimum_length error, expect int between 1 and image_size(smaller side) got {}".format(minimum_length))
+    
+    output = []
+    for c in range(n_crop):
+        for t in range(max_try):
+            length = np.random.randint(minimum_length, np.min(img.shape[:2])+1)
+            y = np.random.randint(img.shape[0]+1-length)
+            x = np.random.randint(img.shape[1]+1-length)
+
+            centers = (starts + ends)/2
+            center_in_square_bool = (centers[:,0]>=x)*(centers[:,0]<x+length)*(centers[:,1]>=y)*(centers[:,1]<y+length)
+            box_in_square_ratio = (ends - starts)[center_in_square_bool].max(axis=1)/length
+            if not ((box_in_square_ratio>=avoid_ratio[0])*(box_in_square_ratio<=avoid_ratio[1])).any():
+                
+                crop_img = crop_square(img, (x, y), length)
+                
+                if resize>0:
+                    crop_img = cv2.resize(crop_img, (resize, resize)) 
+                
+                if flip:
+                    if np.random.rand()>0.5:
+                        crop_img = cv2.flip(crop_img, 1)
+                        crop_box_center[0] = 1- crop_box_center[0]
+                
+                output.append(crop_img)
+                break
+    
+    return output
