1df110c01512593360f06f4dd1434447ca0f1539
Dogiko <dogiko@Dogikode-MacBook-Pro.local>
2019-03-06 15:02:22 +0800

    modify index

--- bounding_box_argumentation.py
+++ bounding_box_argumentation.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import torch
 from cheak_tools import cheak_type, cheak_interval
 
 def ext_black(img, extend):
@@ -93,4 +94,4 @@ def random_crop_square_nothing(img, starts, ends, n_crop=1, resize=-1, avoid_rat
     return output
 
 def img_to_torch(numpy_img):
-    return torch.tensor(np.transpose(numpy_img.astype(np.float64), (0,2,3,1))/127.5 - 1)
+    return torch.tensor(np.transpose(numpy_img.astype(np.float64), (0,3,1,2))/127.5 - 1)
